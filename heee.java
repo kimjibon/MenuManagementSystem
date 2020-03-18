bef3db1d4d20f99d901a2cb9360cd3345696571d
ioi <ioi@DESKTOP-0RT0ANE>
2020-03-18 13:15:44 +0900

    Creat a menu

--- heee.java
+++
@@ -1,8 +0,0 @@
-package heee;
-
-public class heee {
-	public static void main(String[] arguments) {
-		System.out.println("Hello,world!");
-		System.out.println("I am a new java prgrammer!");
-	}
-}
---
+++ src/heee.java
@@ -0,0 +1,11 @@
+public class heee {
+	public static void main(String[] arguments) {
+		System.out.println(" *** Menu Management System Menu *** ");
+		System.out.println(" 1. Add Menu");
+		System.out.println(" 2. Delete Menu");
+		System.out.println(" 3. Edit Menu");
+		System.out.println(" 4. View Menu");
+		System.out.println(" 5. Exit");
+		System.out.println("Select on number between 1 - 5 ");
+	}
+}
