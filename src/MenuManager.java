e8a6171519e76c60198b4dc91b0e80b770c4ae0b
kimjibon <62320363+kimjibon@users.noreply.github.com>
2020-03-26 16:58:58 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,11 +1,66 @@
-public class heee {
+import java.util.Scanner;
+
+public class MenuManager {
 	public static void main(String[] arguments) {
-		System.out.println(" *** Menu Management System Menu *** ");
-		System.out.println(" 1. Add Menu");
-		System.out.println(" 2. Delete Menu");
-		System.out.println(" 3. Edit Menu");
-		System.out.println(" 4. View Menu");
-		System.out.println(" 5. Exit");
-		System.out.println("Select on number between 1 - 5 ");
+		Scanner input = new Scanner(System.in);
+
+		int num = 5;
+		while (num !=6){
+			System.out.println(" *** Menu Management System Menu *** ");
+			System.out.println(" 1. Add Menu");
+			System.out.println(" 2. Delete Menu");
+			System.out.println(" 3. Edit Menu");
+			System.out.println(" 4. View Menu");
+			System.out.println(" 5. Show a Menu");
+			System.out.println(" 5. Exit");
+			System.out.println("Select on number between 1 - 5 ");
+			num = input.nextInt();
+			if (num==1) {
+				addMenu();
+			}
+			else if (num==2) {
+				deleteMenu();
+			}
+			else if (num==3) {
+				editMenu();
+			}
+			else if (num==4) {
+				viewMenu();
+			}
+			else {
+				continue;
+			}
+		}
+	}
+	public static void addMenu() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Menu Name : ");
+		String Menuname = input.next();
+		System.out.print("Menu Price : ");
+		int Menuprice = input.nextInt();
+		System.out.print("Menu Description : ");
+		String Enterdeleter1 = input.nextLine();
+		String Menuintro = input.nextLine();
+		System.out.print("ingredient : ");
+		String ing = input.nextLine();
+
+	}
+	public static void deleteMenu() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Menu Name : ");
+		String Menuname = input.next();
+
+	}
+	public static void editMenu() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Menu Name : ");
+		String Menuname = input.next();
+
+	}
+	public static void viewMenu() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Menu Name : ");
+		String Menuname = input.next();
+
 	}
 }
