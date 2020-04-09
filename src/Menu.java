409666030f92d5234a567d584bb8bbc699f6a7b9
ioi <ioi@DESKTOP-0RT0ANE>
2020-04-10 02:09:23 +0900

    Use array.

--- src/Menu.java
+++ src/Menu.java
@@ -6,13 +6,15 @@ public class Menu {
 	String intro;
 	String ing;
 
+
 	public Menu() {
 
 	}
 
-	public Menu (String menu, int price) {
-		this.menu = menu;
+	public Menu(String menu, int price) {
+		this.menu= menu;
 		this.price = price;
+
 	}
 	public Menu (String menu, int price, String intro, String ing) {
 		super();
@@ -20,6 +22,7 @@ public class Menu {
 		this.price = price;
 		this.intro = intro;
 		this.ing = ing;
+
 	}
 
 	public void printInfo() {
